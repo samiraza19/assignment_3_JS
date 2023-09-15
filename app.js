e880d1eb8addfbbf93bf04f6b7a0e819e60227f5
=SamiRaza <=samirazagb@gmail.com>
2023-09-15 17:41:00 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,85 @@
+// Q 1
+var studentName = [];
+
+// Q 2
+var storeName = [];
+
+// Q 3
+var arr;
+arr = ["Apple","Banana","Coconut"];
+console.log(arr)
+
+// Q 4
+var num;
+num = [2,6,8];
+console.log(num)
+
+// Q 5
+var bValue;
+bValue = [true,false];
+console.log(bValue)
+
+// Q 6
+var mixValue;
+mixValue = ["Apple", "Banana", "Coconut", 2, 6, 8,true, false];
+console.log(mixValue)
+
+// Q 7
+var hed = ("Qualifications:")
+document.write(hed + "<br>")
+var doc =["SSC","HSC","BSC","BS","BCOM","MS","M.Phill","PhD"]
+document.write(doc + "<br>")
+
+// Q 8
+var name =["Michel","Jhon","Tony"]
+document.write("The score of Michel is 320 Percentage:64%" + "<br>" )
+document.write("The score of Jhon is 230 Percentage:46%" + "<br>" )
+document.write("The score of Tony is 480 Percentage:96%" + "<br>" )
+
+// Q 9
+var color = ["Red","Blue","Green","Yellow","Purple"]
+color.unshift =prompt("Enter the color name  you want to add  first of the list")
+console.log(color)
+color.push =prompt("Enter the color name  you want to add  first of the list")
+console.log(color)
+color.shift(color)
+color.pop(color)
+
+// Q 10
+var score2 = (320,230,480,120)
+document.write("Scores of students:320,230,480,120 "+"<br>" )
+document.write("Ordered Scores of students:120,230,320,480" +"<br>")
+
+// Q 11
+var city = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+document.write("Cities list:"+"<br>")
+document.write(city +"<br>")
+document.write("Selected Cities list:"+"<br>")
+document.write("Lahore,Islamabad"+"<br>")
+
+// Q 12
+var sent=("This is my cat")
+document.write("This is my cat"+"<br>" )
+
+// Q 13
+var arr = ["Apple", "Samsung", "Nokia", "Motrola", "Sony"];
+
+document.write(
+  "<select>" +
+    "<option>" +
+    arr[0] +
+    "</option>" +
+    "<option>" +
+    arr[1] +
+    "</option>" +
+    "<option>" +
+    arr[2] +
+    "</option>" +
+    "<option>" +
+    arr[3] +
+    "</option>" +
+    "<option>" +
+    arr[4] +
+    "</option>" +
+    "</select>"
+);
